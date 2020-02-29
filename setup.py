e088bd1561fab608f59771e59d6cb1eabd3d2c83
宋伟(songwei) <songwei@songwei.io>
2020-03-01 00:00:03 +0800

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unified_diff',
-    version='0.1.0',
+    version='0.2.0',
     author='songwei',
     author_email='songwei@songwei.io',
     url='https://github.com/xdusongwei/unified_diff',
